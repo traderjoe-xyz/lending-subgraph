06371e281e48a4881c2dc0f2e9714987e3097668
Chris Hadiono <hadionoc@gmail.com>
2021-10-11 11:18:26 -0400

    add function for creating the liquidation day data

--- src/entities/liquidation-day-data.ts
+++ src/entities/liquidation-day-data.ts
@@ -4,34 +4,16 @@ import { zeroBD } from '../mappings/helpers'
 import { LiquidateBorrow } from '../types/templates/JToken/JToken'
 
 export function updateLiquidationDayData(event: LiquidateBorrow): LiquidationDayData {
-  const timestamp = event.block.timestamp.toI32()
-
-  const day = timestamp / 86400
-
-  const id = event.address
-    .toHexString()
-    .concat('-')
-    .concat(BigInt.fromI32(day).toString())
   const mintID = event.transaction.hash
     .toHexString()
     .concat('-')
     .concat(event.transactionLogIndex.toString())
 
-  const market = Market.load(event.address.toHexString())
-  let liquidationDayData = LiquidationDayData.load(id)
   const liquidationBlock = LiquidationEvent.load(mintID)
 
-  if (liquidationDayData === null) {
-    liquidationDayData = new LiquidationDayData(id)
-    liquidationDayData.date = day * 86400
-    liquidationDayData.amount = zeroBD
-    liquidationDayData.amountUSD = zeroBD
-    liquidationDayData.txCount = 0
-    liquidationDayData.jTokenSymbol = liquidationBlock.jTokenSymbol
-    liquidationDayData.underlyingSymbol = liquidationBlock.underlyingSymbol
-    liquidationDayData.underlyingRepayAmount = zeroBD
-    liquidationDayData.liquidationEvents = []
-  }
+  let liquidationDayData = createLiquidationDayData(event, liquidationBlock)
+
+  const market = Market.load(event.address.toHexString())
 
   const liquidationBlockAmountUSD = liquidationBlock.underlyingRepayAmount.times(
     market.underlyingPriceUSD,
@@ -49,4 +31,34 @@ export function updateLiquidationDayData(event: LiquidateBorrow): LiquidationDay
   liquidationDayData.save()
 
   return liquidationDayData as LiquidationDayData
+}
+
+function createLiquidationDayData(
+  event: LiquidateBorrow,
+  liquidationBlock: LiquidationEvent,
+): LiquidationDayData {
+  const timestamp = event.block.timestamp.toI32()
+
+  const day = timestamp / 86400
+
+  const id = event.address
+    .toHexString()
+    .concat('-')
+    .concat(BigInt.fromI32(day).toString())
+
+  let liquidationDayData = LiquidationDayData.load(id)
+
+  if (liquidationDayData === null) {
+    liquidationDayData = new LiquidationDayData(id)
+    liquidationDayData.date = day * 86400
+    liquidationDayData.amount = zeroBD
+    liquidationDayData.amountUSD = zeroBD
+    liquidationDayData.txCount = 0
+    liquidationDayData.jTokenSymbol = liquidationBlock.jTokenSymbol
+    liquidationDayData.underlyingSymbol = liquidationBlock.underlyingSymbol
+    liquidationDayData.underlyingRepayAmount = zeroBD
+    liquidationDayData.liquidationEvents = []
+  }
+
+  return liquidationDayData
 }
